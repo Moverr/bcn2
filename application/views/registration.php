36139865d06e94436ada9c90fece55aec0d3a89c
Muyinda Rogers <moverr@gmail.com>
2018-10-20 14:48:52 +0300

    Debugging :

--- application/views/registration.php
+++ application/views/registration.php
@@ -2,10 +2,12 @@
     include 'includes/header.php';
     ?>
  
- <script  src="scripts/registration.js"></script>
+ <script  src="scripts/registration.js"></script> 
+ <link rel="stylesheet" href="scripts/registration.css" crossorigin="anonymous" />
+
 <div class="container-fluid">
   <div class="row" id="content" >
-  <div class="container-fluid registration_form"  >
+  <div class="container-fluid registration_form hide_form_div registration_panel "  >
                     <div class="row">
                         <div class="col-md-12">
                         <h1>CREATE AN ACCOUNT  </h1>
@@ -56,9 +58,9 @@
                     </div>
     </div>
 
-	 <div class="container-fluid login_form "  >
+	 <div class="container-fluid show_form_div login_panel "  >
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-8">
                         <h1> LOGIN ACCOUNT  </h1>
                             <form role="form">
                                 <div class="form-group">
@@ -80,12 +82,32 @@
                                
                                 
 
-                                <button type="button" onClick="javascript:handleRegistration()" class="btn btn-primary">
+                                <button type="button" id="loginFormButton"   class="btn btn-primary">
                                     Submit 
                                 </button> &nbsp;&nbsp;&nbsp;
-                                <a href="#"> Already have an Account</a>
+                                <a href="#" id="registerAccount"> Register an account </a>
                             </form>
-                        </div>
+						</div>
+						
+						<div class="col-md-4">
+                        <h4> FORGOT PASSWORD  </h4>
+                            <form role="form">
+                                <div class="form-group">
+                                    
+                                    <label htmlFor="userEmailAddress">
+                                       Emailaddress: 
+                                    </label>
+                                    <input type="text" class="form-control" id="userEmailAddress" />
+                                </div>
+
+                                 
+                                <button type="button" onClick="javascript:handleRegistration()" id="resetFormButton" class="  btn btn-primary">
+                                    Reset 
+                                </button>
+                             
+                            </form>
+						</div>
+						
                     </div>
                     <div class="row">
                         <div class="col-md-12">
