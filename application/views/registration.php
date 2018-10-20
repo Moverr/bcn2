74cdc0696abfa9ebfe645cbdf0c494e16e5110e6
Muyinda Rogers <moverr@gmail.com>
2018-10-20 14:07:22 +0300

    Registration/login form :

--- application/views/registration.php
+++ application/views/registration.php
@@ -5,7 +5,7 @@
  <script  src="scripts/registration.js"></script>
 <div class="container-fluid">
   <div class="row" id="content" >
-  <div class="container-fluid"  >
+  <div class="container-fluid registration_form"  >
                     <div class="row">
                         <div class="col-md-12">
                         <h1>CREATE AN ACCOUNT  </h1>
@@ -54,7 +54,45 @@
                         <div class="col-md-12">
                         </div>
                     </div>
-        </div>
+    </div>
+
+	 <div class="container-fluid login_form "  >
+                    <div class="row">
+                        <div class="col-md-12">
+                        <h1> LOGIN ACCOUNT  </h1>
+                            <form role="form">
+                                <div class="form-group">
+                                    
+                                    <label htmlFor="loginUsername">
+                                       Username: 
+                                    </label>
+                                    <input type="text" class="form-control" id="loginUsername" />
+                                </div>
+
+                                 <div class="form-group">
+                                    
+                                    <label htmlFor="loginPassword">
+                                       Password : 
+                                    </label>
+                                    <input type="loginPassword" class="form-control" id="loginPassword" />
+                                </div>
+
+                               
+                                
+
+                                <button type="button" onClick="javascript:handleRegistration()" class="btn btn-primary">
+                                    Submit 
+                                </button> &nbsp;&nbsp;&nbsp;
+                                <a href="#"> Already have an Account</a>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                        </div>
+                    </div>
+    </div>
+
 
   </div>
 </div>
