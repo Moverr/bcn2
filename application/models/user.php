debddb6d877af8779c79669bab7e458a50f86933
Muyinda Rogers <moverr@gmail.com>
2018-10-20 23:24:50 +0300

    Debugging :

--- application/models/user.php
+++ application/models/user.php
@@ -4,10 +4,29 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User extends CI_Model
 {
-    public function add_data($queryCode, $queryData = array())
+    public function add($person_id, $details = array())
     {
-        $this->db->query($this->get_query_by_code($queryCode, $queryData));
+        $added_data = array(
+            'username' => $details['username'],
+            'emailaddress' => htmlentities($details['emailaddress'], ENT_QUOTES),
+            'password' => htmlentities($details['password'], ENT_QUOTES),
+            'status' => 'ACTIVE',
+        );
 
-        return $this->db->insert_id();
+        if (!empty($details['id'])) {
+            $added_data['id'] = $details['id'];
+
+            $personId = $this->_query_reader->run('update_post', $added_data);
+            $data['boolean'] = true;
+            $data['msg'] = 'SUCCESS:  Record Updated  Succesfully ';
+        } else {
+            $personId = $this->_query_reader->add_data('add_post', $added_data);
+            if ($personId) {
+                $data['boolean'] = true;
+                $data['msg'] = 'SUCCESS:  Record Saved Succesfully ';
+            }
+        }
+
+        return $data;
     }
 }
