b9809f91432f66acebfec8077d988626ea04c109
Muyinda Rogers <moverr@gmail.com>
2018-10-20 03:13:17 +0300

    Debugging :

---
+++ application/views/includes/footer.php
@@ -0,0 +1,3 @@
+<script src="./scripts//dist/react/react.development.js" crossorigin></script>
+    <script src="./scripts/dist/react/react-dom.development.js" crossorigin></script>	
+	 <script src="./scripts/like_button.js"></script>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,43 @@
+<?php
+    include 'includes/header.php';
+    ?>
+<body>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+
+	<div id="body">
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/Welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : ''; ?></p>
+</div>
+
+
+h2>Add React in One Minute</h2>
+    <p>This page demonstrates using React with no build tooling.</p>
+    <p>React is loaded as a script tag.</p>
+
+    <!-- We will put our React component inside this div. -->
+    <div id="like_button_container"></div>
+
+ 	<script src="./scripts/dist/bootstrap/js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="./scripts/dist/bootstrap/js/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="./scripts/dist/bootstrap/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+ 
+
+ 
+
+<?php
+    include 'includes/footer.php';
+    ?>
+</body>
+</html>
