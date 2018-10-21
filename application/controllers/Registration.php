b2389d9b3775a3b3fa79f6db67ea59936ac2cd30
Muyinda Rogers <moverr@gmail.com>
2018-10-21 18:37:15 +0300

    : Debugging :

--- application/controllers/Registration.php
+++ application/controllers/Registration.php
@@ -54,4 +54,49 @@ class Registration extends CI_Controller
             echo   $response['message'];
         }
     }
+
+    public function login()
+    {
+        $data = filter_forwarded_data($this);
+
+        if (!empty($_POST)) {
+            //Is form verified?
+            if ($this->input->post('verified')) {
+                //Is user verified?
+                $results = $this->user->is_valid_account(array('login_name' => trim($this->input->post('loginusername')), 'login_password' => trim($this->input->post('loginpassword'))));
+                if ($results['boolean']) {
+                    // $this->load->model('_permission');
+                    //If so, assign permissions and redirect to their respective dashboard
+                    // $this->native_session->set('__permissions', $this->_permission->get_user_permission_list($results['user_id']));
+                    //Log sign-in event
+                    // $this->_logger->add_event(array('log_code' => 'user_login', 'result' => 'success', 'details' => 'userid='.$results['user_id'].'|username='.trim($this->input->post('loginusername'))));
+
+                    $this->session->sess_expiration = 900; // 15 mins
+                    $this->session->sess_expire_on_close = false;
+
+                // Go to the user dashboard
+                    // redirect(base_url().'message/inbox');
+                }
+                // Invalid credentials
+                else {
+                    $this->_logger->add_event(array('log_code' => 'user_login', 'result' => 'fail', 'details' => 'username='.trim($this->input->post('loginusername'))));
+                    $data['msg'] = 'WARNING: Invalid login details.';
+                    // $this->load->view('account/login', $data);
+                    echo $data;
+                }
+            } else {
+                $data['msg'] = 'ERROR: Your submission could not be verified.';
+
+                // $this->load->view('account/login', $data);
+                echo $data;
+            }
+        }
+        // If already logged in, log out of current session
+        elseif ($this->native_session->get('__user_id')) {
+            $this->logout($this->native_session->get('__user_id'));
+            $data['msg'] = 'You have been logged out.';
+            echo $data;
+            // $this->load->view('account/login', $data);
+        }
+    }
 }
--- application/models/user.php
+++ application/models/user.php
@@ -27,4 +27,30 @@ class User extends CI_Model
 
         return $data;
     }
+
+    public function is_valid_account($accountDetails)
+    {
+        $boolean = false;
+        $userId = '';
+
+        $user = $this->query_reader->get_row_as_array('get_user_by_name_and_pass', array('login_name' => $accountDetails['login_name'], 'login_password' => sha1($accountDetails['login_password'])));
+        if (!empty($user)) {
+            $boolean = true;
+            $userId = $user['id'];
+
+            //Set the user's session variables
+            $this->native_session->set('user_id', $user['id']);
+            $this->native_session->set('email_address', $user['emailaddress']);
+            // if (!empty($user['telephone'])) {
+            //     $this->native_session->set('telephone', $user['telephone']);
+            // }
+            // $this->native_session->set('permission_group', $user['permission_group_id']);
+            $this->native_session->set('username', $user['username']);
+            // $this->native_session->set('last_name', $user['last_name']);
+            // $this->native_session->set('gender', $user['gender']);
+            // $this->native_session->set('date_of_birth', $user['date_of_birth']);
+        }
+
+        return array('boolean' => $boolean, 'user_id' => $userId);
+    }
 }
